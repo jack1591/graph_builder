b4ac8966b05a4a75d7089a0cd5c6721bf5c59a6e
Artem <jack.tag@mail.ru>
2024-10-12 11:06:25 +0300

    proverka

---
+++ proj/build/CMakeFiles/utest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utest.dir/src/utest.cpp.obj"
+  "CMakeFiles/utest.dir/src/utest.cpp.obj.d"
+  "libutest.dll.a"
+  "utest.exe"
+  "utest.exe.manifest"
+  "utest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
